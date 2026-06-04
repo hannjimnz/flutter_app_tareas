6b7091b070a2d694ede4b65633906a045a180cdf
hannjimnz <hannjimnz@gmail.com>
2026-06-04 07:39:23 -0600

    featt: clase y atributos para las tareas

--- lib/tarea_modelo.dart
+++ lib/tarea_modelo.dart
@@ -6,8 +6,15 @@ class Tarea {
   final String id;
   final String titulo;
   bool estaCompletada; // esta no es final porque cambiara entre verdadero y Falso
+
+  //haremos nuestro constructor 
+  //se usa required null safety ya que no se  puede tener una tarea sin un id o un titulo
+  Tarea({
+  required this.id, 
+  required this.titulo, 
+  this.estaCompletada = false,// por defecto las tareas nuevas empiezan sin completar 
+});
+
 }
 
-Tarea({
-  required this.id, 
-});
+
